947b9e1e4975410aaaed6ea35baeea5ad0c02af1
archqob <archqob@gmail.com>
2018-03-11 16:18:54 +0200

    add uninstall script

---
+++ src/uninstall.sh
@@ -0,0 +1,21 @@
+ #!/bin/bash
+
+sudo rm /usr/bin/kontrol
+sudo rm "/usr/share/icons/hicolor/16x16/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/20x20/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/22x22/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/24x24/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/32x32/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/36x36/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/40x40/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/48x48/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/64x64/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/72x72/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/96x96/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/128x128/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/192x192/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/256x256/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/512x512/apps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/1024x1024/apps/kontrol.png"
+sudo rm "/usr/share/pixmaps/kontrol.png"
+sudo rm "/usr/share/icons/hicolor/scalable/apps/kontrol.svg"
